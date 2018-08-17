25629332c0cf7e6dba0f43080eb053a1c5c5e0c0
gausner <gausner@list.ru>
2018-08-17 12:24:02 -0400

    initial plugin with workbench repo

---
+++ plugin/src/main/java/com/myspace/MyMojo.java
@@ -0,0 +1,25 @@
+package com.myspace;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+
+@Mojo(name = "hello")
+public class MyMojo extends AbstractMojo {
+    @Parameter(property = "msg", defaultValue = "from maven")
+    private String msg;
+
+    public void execute()
+            throws MojoExecutionException {
+        getLog().info("Hello " + msg);
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+}
